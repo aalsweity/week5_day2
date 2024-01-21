11fe071795cb307a685e163ca21884d541e295ac
Anas Al-Sweity <aalsweity@gmail.com>
2024-01-21 15:32:28 -0500

    second commit

--- week5_day2.sql
+++ week5_day2.sql
@@ -1,22 +1,27 @@
-CREATE TABLE "Movie" (
-  "movie_id" SERIAL,
+DROP TABLE IF EXISTS Movie CASCADE;
+CREATE TABLE Movie (
+  "movie_id" SERIAL PRIMARY KEY,
   "movie_title" VARCHAR,
   "genre" VARCHAR,
   "rating" VARCHAR,
   "time_length" INTEGER
 );
 
-CREATE TABLE "Customer" (
-  "customer_id" SERIAL,
+DROP TABLE IF EXISTS Customer CASCADE;
+CREATE TABLE Customer (
+  "customer_id" SERIAL PRIMARY KEY,
   "first_name" VARCHAR,
   "last_name" VARCHAR,
   "email" VARCHAR,
   "phone_number" VARCHAR
 );
 
-CREATE TABLE "Ticket" (
-  "ticket_id" SERIAL,
+DROP TABLE IF EXISTS Ticket CASCADE;
+CREATE TABLE Ticket (
+  "ticket_id" SERIAL PRIMARY KEY,
   "customer_id" INTEGER,
   "movie_id" INTEGER,
-  "date" TIMESTAMP
+  "date" TIMESTAMP,
+  FOREIGN KEY (movie_id) REFERENCES Movie(movie_id) ON DELETE CASCADE,
+  FOREIGN KEY (customer_id) REFERENCES Customer(customer_id) ON DELETE CASCADE
 );
